966ad5d9eccb48105e19410d681e656e4c4993aa
Jarwo Hoki <153470917+jarwohok@users.noreply.github.com>
2024-03-22 06:35:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-186fd7fd-2f03-4380-939d-759f8c4fc99a/)
 
 
 echo "$curl_output" >> Update.tsx
